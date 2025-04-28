83aef9b3922b60d42e63c3f4bcc3a1bce6703044
szyman <szyman040395@gmial.com>
2025-04-28 15:41:41 +0200

    Refactor and extend UI with typography and themes support
    Introduced a new typography system with reusable components (H1, H2, etc.) in `ui/typography`. Added theme support with light and dark modes, updating Tailwind CSS config and global styles for theming. Integrated `nativewind` for unified styling and enhanced project code structure with utility and constant modules.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { H1 } from "~/components/ui/typography"
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <H1>Edit app/index.tsx to edit this screen.</H1>
     </View>
   );
 }
